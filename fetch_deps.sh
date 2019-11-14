f2570170be8eabdc664d4894fa014f278a16f300
paidforby <ggallo102@gmail.com>
2019-11-14 08:57:22 -0500

    prepare esp32 branch for merge into master

--- fetch_deps.sh
+++ fetch_deps.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-rm -rf makeEspArduino arduino-esp32 libs Makefile
+BOARD=$1
+
+if [ -d makeEspArduino ]; then rm -rf makeEspArduino; fi
+if [ -d esp8266-arduino ]; then rm -rf esp8266-arduino; fi
+if [ -d arduino-esp32 ]; then rm -rf arduino-esp32; fi
+if [ -d libs ]; then rm -rf libs; fi
 
 # get makeEspArduino
 git clone https://github.com/plerup/makeEspArduino
@@ -11,27 +16,42 @@ git checkout tags/4.18.0
 cp makeEspArduino.mk ../Makefile
 cd ../
 
-# https://dl.espressif.com/dl/package_esp32_index.json, http://arduino.esp8266.com/stable/package_esp8266com_index.json
-
-# install arduino-esp32 build environment
-git clone https://github.com/espressif/arduino-esp32 arduino-esp32
-cd arduino-esp32/
-git checkout tags/1.0.4
-cd tools/
-python get.py
-cd ../../
-
-# install ESPAsyncTCP and ESPAsyncWebserver
 mkdir -p libs
+
+if [ $BOARD == "esp8266" ]; then
+    # install esp8266-arduino build environment
+    git clone https://github.com/esp8266/Arduino.git esp8266-arduino
+    cd esp8266-arduino/
+    git checkout tags/2.4.0-rc1
+    cd tools/
+    python get.py
+    cd ../../
+    # install ESPAsyncTCP
+    cd libs/
+    git clone https://github.com/me-no-dev/ESPAsyncTCP
+    cd ESPAsyncTCP/
+    git checkout 991f855109d8038ed2cf0b5fb89792fcfa23549c
+    cd ../../
+fi
+
+if [ $BOARD == "esp32" ]; then
+    # install arduino-esp32 build environment
+    git clone https://github.com/espressif/arduino-esp32 arduino-esp32
+    cd arduino-esp32/
+    git checkout tags/1.0.4
+    cd tools/
+    python get.py
+    cd ../../
+    # install ESPAsyncTCP
+    cd libs/
+    git clone https://github.com/me-no-dev/AsyncTCP
+    # see https://community.platformio.org/t/compile-error-when-trying-to-use-the-asynctcp-library/8460
+    cd AsyncTCP/
+    git checkout c9df7cdda6302f509db9c09d9a514a45f9392105
+    cd ../../
+fi
+
 cd libs/
-
-git clone https://github.com/me-no-dev/AsyncTCP
-# see https://community.platformio.org/t/compile-error-when-trying-to-use-the-asynctcp-library/8460
-
-cd AsyncTCP/
-git checkout c9df7cdda6302f509db9c09d9a514a45f9392105
-cd ../
-
 git clone https://github.com/me-no-dev/ESPAsyncWebServer
 cd ESPAsyncWebServer/
 git checkout f13685ee97675be2ac9502d177d3024ebc49c1e0
@@ -51,3 +71,5 @@ git clone https://github.com/paidforby/AsyncSDServer
 cd AsyncSDServer/
 git checkout 13375c6be978cb34180378ecf4042a3a4a1f5eab
 cd ../../
+
+
